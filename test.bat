faaaa13abe3fd70d77f0bd68b8b8d1c33edef59b
chenqiyu <390739790@qq.com>
2023-05-24 17:46:14 +0800

    add

--- test.bat
+++ test.bat
@@ -1,11 +1,10 @@
 set PATH="D:\cygwin64\bin\";%PATH%
 @rem Test HTTP proxy
 @rem 
-@rem node ./bin/proxy --host=0.0.0.0 --port=8080 --protocol=http;
-
+@rem 
+node ./bin/proxy --host=0.0.0.0 --port=8080 --protocol=http
 @rem Test HTTPS proxy
 @rem openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem;
-@rem 
-node ./bin/proxy --host=0.0.0.0 --port=8080 --protocol=https
+@rem node ./bin/proxy --host=0.0.0.0 --port=8080 --protocol=https
 
 pause
